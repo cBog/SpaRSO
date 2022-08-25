91b7c6eb0c15f42b6d89c8bf3709c4a904cdb2e3
cBog <P17158321@my365.dmu.ac.uk>
2022-08-25 17:10:42 +0100

    AUTO_RUN: Experiment 0.1: SGD baseline
     from 8ff75ec23fca8cc74f3ebac2cedad118a987e9b5

--- optimisers.py
+++ optimisers.py
@@ -62,7 +62,8 @@ class StandardSGD(Optimiser):
   def __init__(self, model, epochs):
     super(StandardSGD, self).__init__(model)
     self.epochs = epochs
-    self.sgd_optimiser = tf.keras.optimizers.Adam(0.001)
+    # self.sgd_optimiser = tf.keras.optimizers.Adam(0.001)
+    self.sgd_optimiser = tf.keras.optimizers.Adam(0.006)
 
   @tf.function
   def train_step_gradients(self, x, y):
