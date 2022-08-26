61f514dc8d4b9243cc7cca9ee1b14f0e2e86e075
cBog <P17158321@my365.dmu.ac.uk>
2022-08-26 16:15:41 +0100

    added script to plot fwd counts against accuracy

---
+++ plot_foward_counts_gen_script.py
@@ -0,0 +1,18 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import sys
+
+# usage: call this script from top of repo with a space separated list of log ids
+
+for log_id in sys.argv[1:]:
+    fwd_cnts = np.load(f"logs/{log_id}/training_forwards_counts.npy")
+    train_accs = np.load(f"logs/{log_id}/training_acc_results.npy")
+    val_accs = np.load(f"logs/{log_id}/val_acc_results.npy")
+    fig = plt.figure()
+    plt.title("training and validation accuracy against forward execution counts")
+    plt.xlabel("Count of calls to forward function")
+    plt.ylabel("accuracy")
+    plt.plot(fwd_cnts, train_accs, label="training accuracy")
+    plt.plot(fwd_cnts, val_accs, label="validation accuracy")
+    plt.legend(loc="upper left")
+    fig.savefig(f"logs/{log_id}/plot_accs_fwd_cnts.png")
