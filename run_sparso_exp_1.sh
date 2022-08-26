3eeb3e877aa85a349e6c26b00e40545dbb6f8b88
cBog <P17158321@my365.dmu.ac.uk>
2022-08-26 10:33:45 +0100

    added experiment 1 SpaRSO config

--- run_sparso_exp_1.sh
+++ run_sparso_exp_1.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-description="${@:1}"
+description="Experiment 1.0: improve only"
 echo $description
 
-python train.py --run-description "${description}" --batch-size 1024 --optimiser spaRSO --opt-iters 6 --initial-density 0.05 --maximum-density 0.25 --initial-prune-factor 0.2 --swap-proportion 0.2 --consider-zero-improve --batch-mode every_iteration --norm-type none --const-norm-weights --phases improve prune regrow replace
-
+python train.py --run-description "${description} initial density = 0.7" --batch-size 1024 --optimiser spaRSO --opt-iters 10 --initial-density 0.7 --maximum-density 1.0 --batch-mode every_weight --norm-type batch --const-norm-weights --phases improve
+python train.py --run-description "${description} initial density = 0.6" --batch-size 1024 --optimiser spaRSO --opt-iters 10 --initial-density 0.6 --maximum-density 1.0 --batch-mode every_weight --norm-type batch --const-norm-weights --phases improve
+python train.py --run-description "${description} initial density = 0.5" --batch-size 1024 --optimiser spaRSO --opt-iters 10 --initial-density 0.5 --maximum-density 1.0 --batch-mode every_weight --norm-type batch --const-norm-weights --phases improve
+python train.py --run-description "${description} initial density = 0.4" --batch-size 1024 --optimiser spaRSO --opt-iters 10 --initial-density 0.4 --maximum-density 1.0 --batch-mode every_weight --norm-type batch --const-norm-weights --phases improve
+python train.py --run-description "${description} initial density = 0.3" --batch-size 1024 --optimiser spaRSO --opt-iters 10 --initial-density 0.3 --maximum-density 1.0 --batch-mode every_weight --norm-type batch --const-norm-weights --phases improve
+python train.py --run-description "${description} initial density = 0.2" --batch-size 1024 --optimiser spaRSO --opt-iters 10 --initial-density 0.2 --maximum-density 1.0 --batch-mode every_weight --norm-type batch --const-norm-weights --phases improve
+python train.py --run-description "${description} initial density = 0.1" --batch-size 1024 --optimiser spaRSO --opt-iters 10 --initial-density 0.1 --maximum-density 1.0 --batch-mode every_weight --norm-type batch --const-norm-weights --phases improve
