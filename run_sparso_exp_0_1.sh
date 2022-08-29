b0fd8e50360eca3b967158384d3e7bdb4b5f61c3
cBog <P17158321@my365.dmu.ac.uk>
2022-08-29 09:52:32 +0100

    try larger bs for SGD

--- run_sparso_exp_0_1.sh
+++ run_sparso_exp_0_1.sh
@@ -3,5 +3,5 @@
 description="Experiment 0.1: SGD baseline"
 echo $description
 
-python train.py --run-description "${description}" --batch-size 32 --optimiser SGD --epochs 10
+python train.py --run-description "${description}" --batch-size 128 --optimiser SGD --epochs 11
 
--- run_sparso_exp_0_2.sh
+++ run_sparso_exp_0_2.sh
@@ -3,9 +3,9 @@
 description="Experiment 0.2: SGD baseline with pruning max 0.8"
 echo $description
 
-python train.py --run-description "${description}" --batch-size 32 --optimiser SGD --epochs 10 --run-classic-pruning --pruning-min 0.5 --pruning-max 0.8
+python train.py --run-description "${description}" --batch-size 100 --optimiser SGD --epochs 10 --run-classic-pruning --pruning-min 0.5 --pruning-max 0.8
 
 description="Experiment 0.2: SGD baseline with pruning max 0.9"
 echo $description
 
-python train.py --run-description "${description}" --batch-size 32 --optimiser SGD --epochs 10 --run-classic-pruning --pruning-min 0.5 --pruning-max 0.9
+python train.py --run-description "${description}" --batch-size 100 --optimiser SGD --epochs 10 --run-classic-pruning --pruning-min 0.5 --pruning-max 0.9
