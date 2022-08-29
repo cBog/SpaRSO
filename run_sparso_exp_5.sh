93ca77722503835f625848adb2459ae82bc8fa86
cBog <P17158321@my365.dmu.ac.uk>
2022-08-29 12:48:49 +0100

    use best config settings

--- run_sparso_exp_5.sh
+++ run_sparso_exp_5.sh
@@ -3,4 +3,4 @@
 description="Experiment 5.0: combined phases"
 echo $description
 
-python train.py --run-description "${description}, combined phases" --batch-size 1024 --optimiser spaRSO --opt-iters 5 --swap-proportion 0.3 --initial-density 0.2 --maximum-density 0.3 --initial-prune-factor 0.1 --batch-mode every_weight --norm-type batch --const-norm-weights --phases prune regrow improve replace improve
+python train.py --run-description "${description}, combined phases" --batch-size 1024 --optimiser spaRSO --opt-iters 5 --swap-proportion 0.3 --initial-density 0.2 --maximum-density 0.3 --initial-prune-factor 0.3 --batch-mode every_weight --norm-type batch --const-norm-weights --phases prune regrow improve replace improve
