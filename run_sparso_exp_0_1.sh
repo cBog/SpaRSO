e3056e5c9d627a360902c2a2673301fab5db7318
cBog <P17158321@my365.dmu.ac.uk>
2022-08-29 18:28:16 +0100

    adjustments to SGD config

--- run_sparso_exp_0_1.sh
+++ run_sparso_exp_0_1.sh
@@ -3,5 +3,5 @@
 description="Experiment 0.1: SGD baseline"
 echo $description
 
-python train.py --run-description "${description}" --batch-size 128 --optimiser SGD --epochs 11
+python train.py --run-description "${description}" --batch-size 128 --optimiser SGD --epochs 10
 
