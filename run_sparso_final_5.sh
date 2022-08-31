da00fbe4e294e19a5536f913498fb73288d72be0
cBog <P17158321@my365.dmu.ac.uk>
2022-08-31 17:35:09 +0100

    added final config 5

---
+++ run_sparso_final_5.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+description="Final run replace only different config"
+echo $description
+
+python train.py --run-description "${description}" --batch-size 1024 --optimiser spaRSO --opt-iters 100 --swap-proportion 0.3 --initial-density 0.25 --maximum-density 1.0 --batch-mode every_weight --norm-type batch --const-norm-weights --phases replace improve
